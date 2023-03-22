5981e65b626044a591039320e5eb3983b90be934
Elwilton <elwilton@hotmail.com>
2023-03-22 09:16:23 -0300

    Main

---
+++ Teste Fiotec/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+namespace TesteFiotec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] path = Directory.GetFiles(@"/Users/eltin/Downloads/Apoio Analista| Externo_palavras.txt");
+            foreach (string i in path)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+}
